385765093f5dce1ef09098eba5f26758aebf72a4
TOBIAS JOSE DIGESTANI <49373638@ort>
2025-11-06 09:02:50 -0300

    novena actualizacion

--- obj/Debug/net8.0/Eldertech.AssemblyInfo.cs
+++ obj/Debug/net8.0/Eldertech.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eldertech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c255e6850cb8a22cbde36053bca37c0960a40d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d069fb4fc334f4f29dea30da364dea743648d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eldertech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eldertech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
